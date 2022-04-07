613cffd3577ac7862fd8a9475c6e1c73345df97e
slaheddine2021 <feedcom.finance7@gmail.com>
2022-04-07 11:54:14 +0100

    slah

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -17,5 +18,14 @@ import resources.base;
 
 public class HomePage {
 	
+	
+	public WebDriver driver ;
+	
+	@AfterTest
+	
+	public void initializeDriver() {
+		
+		System.out.println("abe");
+	}
 	
 }
